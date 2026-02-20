c6f5290c3c43a22044e2807d6535295f0b365c32
amirho1 <amirhosseinsalighedar@gmail.com>
2026-02-20 17:22:04 +0330

    Initialize Tailwind Next.js Starter Blog with essential configurations and files. Added environment variables, ESLint, Prettier, and GitHub workflows for CI/CD. Included basic layout and structure for the blog, along with necessary dependencies and configurations for Contentlayer and Tailwind CSS.

---
+++ data/headerNavLinks.ts
@@ -0,0 +1,9 @@
+const headerNavLinks = [
+  { href: '/', title: 'خانه' },
+  { href: '/blog', title: 'وبلاگ' },
+  { href: '/tags', title: 'برچسب‌ها' },
+  { href: '/projects', title: 'پروژه‌ها' },
+  { href: '/about', title: 'درباره' },
+]
+
+export default headerNavLinks
